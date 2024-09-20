889f8f2c027cd3feb4e41a2072999f7ab96710d0
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2024-09-20 21:53:00 +0200

    instert first image for the game

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,14 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 250;
+    height = 90;
+    width = 175;
     img;
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
 
     moveRight(params) {
         console.log('Moving right');
