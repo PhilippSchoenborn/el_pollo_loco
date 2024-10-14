ed445ce8f03528310be97756d85ad32afde33b03
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2024-10-14 12:08:39 +0200

    added fullscreen function when pressing the fullscreen icon

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,21 @@ function closeModal() {
 }
 
 // Close the modal if the user clicks anywhere outside the modal content
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
+    }
+}
+
+function openFullscreen() {
+    const canvasContainer = document.querySelector('.canvas-container');
+    if (canvasContainer.requestFullscreen) {
+        canvasContainer.requestFullscreen();
+    } else if (canvasContainer.mozRequestFullScreen) { // Firefox
+        canvasContainer.mozRequestFullScreen();
+    } else if (canvasContainer.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+        canvasContainer.webkitRequestFullscreen();
+    } else if (canvasContainer.msRequestFullscreen) { // IE/Edge
+        canvasContainer.msRequestFullscreen();
     }
 }
